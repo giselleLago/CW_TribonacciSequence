c95c4202f00e2b4ff6e79dc452f84c9a1dcd27bf
giselleLago <gisellelago25994@gmail.com>
2020-08-04 16:49:03 +0200

    Add project files.

---
+++ CW_TribonacciSequence/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_TribonacciSequence
+{
+    public class Kata
+    {
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            if (n < 3 || signature.Length >= n)
+            {
+                return signature.Take(n).ToArray();
+            }
+
+            var newValue = signature[^1] +  signature[^2] + signature[^3];
+            return Tribonacci(signature.Concat(new[] { newValue }).ToArray(), n);
+        }
+
+    }
+}
---
+++ CW_TribonacciSequence/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            var result = a.Tribonacci(new double[] { 1, 1, 1 }, 10);
+            Console.ReadKey();
+        }
+    }
+}
